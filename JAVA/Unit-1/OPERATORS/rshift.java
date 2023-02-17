afe59e7087cd187369de9ef7c103cd90d164c4e1
Bhavik Agrawal <102372077+BhavikAgrawal1751@users.noreply.github.com>
2023-02-17 17:49:56 +0530

    Add files via upload

---
+++ JAVA/Unit-1/OPERATORS/rshift.java
@@ -0,0 +1,6 @@
+public class rshift{  
+public static void main(String args[]){  
+System.out.println(10>>2);//10/2^2=10/4=2  
+System.out.println(20>>4);//20/2^4=20/16=1  
+System.out.println(20>>3);//20/2^3=20/8=2  
+}}  
---
+++ JAVA/unit-2/Student.java
@@ -0,0 +1,11 @@
+class Student{  
+ //defining fields  
+ int id;
+ String name;   
+ public static void main(String args[]){  
+  	Student s1=new Student();//creating an object of Student  
+  	//Printing values of the object  
+  	System.out.println(s1.id);//accessing member through reference variable  
+  	System.out.println(s1.name);  
+ }  
+}  
