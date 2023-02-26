ffd4b344a0b00645496de1ed220d1a19096cff40
Bhavik Agrawal <102372077+BhavikAgrawal1751@users.noreply.github.com>
2023-02-26 19:23:30 +0530

    Add files via upload

---
+++ NET/Journal/Calculator/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,90 @@
+﻿Public Class Form1
+    Dim value1, value2 As Integer
+    Dim sign As String
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        TextBox1.Text = TextBox1.Text & 1
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        TextBox1.Text = TextBox1.Text & 2
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        TextBox1.Text = TextBox1.Text & 3
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        TextBox1.Text = TextBox1.Text & 4
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        TextBox1.Text = TextBox1.Text & 5
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        TextBox1.Text = TextBox1.Text & 6
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        TextBox1.Text = TextBox1.Text & 7
+    End Sub
+
+    Private Sub Button8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button8.Click
+        TextBox1.Text = TextBox1.Text & 8
+    End Sub
+
+    Private Sub Button9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button9.Click
+        TextBox1.Text = TextBox1.Text & 9
+    End Sub
+
+    Private Sub Button13_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button13.Click
+        TextBox1.Text = TextBox1.Text & 0
+    End Sub
+
+    Private Sub Button10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button10.Click
+        value1 = TextBox1.Text
+        TextBox1.Clear()
+        TextBox1.Focus()
+        sign = "+"
+    End Sub
+
+    Private Sub Button12_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button12.Click
+        value1 = TextBox1.Text
+        TextBox1.Clear()
+        TextBox1.Focus()
+        sign = "-"
+    End Sub
+
+    Private Sub Button14_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button14.Click
+        value1 = TextBox1.Text
+        TextBox1.Clear()
+        TextBox1.Focus()
+        sign = "*"
+    End Sub
+
+    Private Sub Button15_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button15.Click
+        value1 = TextBox1.Text
+        TextBox1.Clear()
+        TextBox1.Focus()
+        sign = "/"
+    End Sub
+
+    Private Sub Button16_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button16.Click
+        value2 = TextBox1.Text
+        If sign = "+" Then
+            TextBox1.Text = value1 + value2
+        ElseIf sign = "-" Then
+            TextBox1.Text = value1 - value2
+        ElseIf sign = "*" Then
+            TextBox1.Text = value1 * value2
+        ElseIf sign = "/" Then
+            TextBox1.Text = value1 / value2
+        End If
+    End Sub
+
+    Private Sub Button11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button11.Click
+        TextBox1.Clear()
+        TextBox1.Focus()
+    End Sub
+End Class
